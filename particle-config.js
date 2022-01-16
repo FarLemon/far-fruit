88569871985652359d499f29313f341d7e555f6e
Joshua Messer <FarLemon.FFC@gmail.com>
2022-01-15 22:44:26 -0500

    more major updates

---
+++ particle-config.js
@@ -0,0 +1,33 @@
+const particlesConfig = {
+  "fpsLimit": 24,
+  "particles": {
+    "color": {
+      "value": "#5fb372"
+    },
+    "move": {
+      "enable": true
+    },
+    "opacity": {
+      "value": {
+        "min": 0,
+        "max": 0.1
+      }
+    },
+    "size": {
+      "value": 250,
+      "random": {
+        "enable": true,
+        "minimumValue": 50
+      },
+    },
+    "number": {
+      "density": {
+        "enable": true,
+        "value_area": 500
+      },
+      "value": 5
+    }
+  }
+};
+
+export default particlesConfig;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,39 @@ import * as React from "react"
 //import { Link } from "gatsby"
 //import { StaticImage } from "gatsby-plugin-image"
 
+import Particles from 'react-tsparticles';
+import particlesConfig from '../../particle-config.js';
+
 import Wrapper from "../components/wrapper"
 import Seo from "../components/seo"
 
 import '../styles/index.scss';
+import '../styles/portfolio.scss';
 
 const IndexPage = () => (
-  <Wrapper>
-    <Seo title="Portfolio" />
-    <h1>Help</h1>
-  </Wrapper>
+  <div>
+    <Wrapper>
+      <Seo title="Portfolio" />
+
+      <section id='home'>
+        <Particles options={particlesConfig} className='particles'></Particles>
+        <h1><span className='highlight'>Joshua Messer</span></h1>
+        <h2>Front-End Web Developer</h2>
+      </section>
+
+      <section id='about'>
+        <h1>About</h1>
+      </section>
+
+      <section id='projects'>
+        <h1>Projects</h1>
+      </section>
+
+      <section id='Contact'>
+        <h1>Contact</h1>
+      </section>
+    </Wrapper>
+  </div>
 )
 
 export default IndexPage
